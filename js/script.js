46c65abe02c4b46b82403182929c0c2225a303f4
stephanieparra <stephanieparrastudio@gmail.com>
2023-03-22 19:48:16 -0700

    Creating variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  console.log(subType);
+});
