6bb13f44a375e3aaf3d6134b48413e21ede061c2
stephanieparra <stephanieparrastudio@gmail.com>
2023-03-22 20:11:15 -0700

    Added function for type and month

--- js/script.js
+++ js/script.js
@@ -6,5 +6,21 @@ var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
   subType = e.target.value;
-  console.log(subType);
+  updateSubscriptionDiv();
 });
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
